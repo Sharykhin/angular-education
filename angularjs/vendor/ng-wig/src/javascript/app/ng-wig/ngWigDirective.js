62f0598b732873b73b8a7878b6d6db9d3904f7a1
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-05-06 09:54:48 +0300

    form-validation

---
+++ angularjs/vendor/ng-wig/src/javascript/app/ng-wig/ngWigDirective.js
@@ -0,0 +1,31 @@
+angular.module('ngWig').directive('ngWig', function () {
+
+      return {
+        scope: {
+          content: '=ngWig'
+        },
+        restrict: 'A',
+        replace: true,
+        templateUrl: 'ng-wig/views/ng-wig.html',
+        link: function (scope, element, attrs) {
+
+          scope.originalHeight = element.outerHeight();
+          scope.editMode = false;
+          scope.autoexpand = !('autoexpand' in attrs) || attrs['autoexpand'] !== 'off';
+          scope.cssPath = scope.cssPath ? scope.cssPath : 'css/ng-wig.css';
+
+          scope.toggleEditMode = function() {
+            scope.editMode = !scope.editMode;
+          };
+
+          scope.execCommand = function (command, options) {
+            if(command ==='createlink'){
+              options = prompt('Please enter the URL', 'http://');
+            }
+            scope.$emit('execCommand', {command: command, options: options});
+          };
+        }
+      }
+    }
+);
+
